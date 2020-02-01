4bcb2afe91dab2f148fb2194892ccd877083283e
Marlon <marlonfsolis@outlook.com>
2020-02-01 13:08:09 -0500

    bootstrap form added.

--- src/components/BsForm/BsForm.component.js
+++ src/components/BsForm/BsForm.component.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Form, Button } from "react-bootstrap";
 
 export default function BsFormComp() {
-    return (
-        <div>
-            <p>Form component</p>
-        </div>
-    )
+   const [form, setForm] = useState({
+      email: { value: "marlon@gmail.com" },
+      password: { value: "password1" }
+   });
+
+   const setFormFieldVal = (name, event) => {
+      let myForm = { ...form };
+      myForm[name].value = event.currentTarget.value;
+      setForm(myForm);
+   };
+
+   const logFormDataHandler = (event) => {
+        event.preventDefault();
+        event.stopPropagation();
+      console.log(form);
+   };
+
+   return (
+      <div>
+         <Form onSubmit={logFormDataHandler}>
+            <Form.Group>
+               <Form.Label>Email</Form.Label>
+               <Form.Control type="email" placeholder="Email" value={form['email'].value} onChange={setFormFieldVal.bind(this, 'email')}></Form.Control>
+            </Form.Group>
+            <Form.Group>
+               <Form.Label>Password</Form.Label>
+               <Form.Control type="password" minLength="6" value={form['password'].value} onChange={setFormFieldVal.bind(this, 'password')}></Form.Control>
+            </Form.Group>
+            <Button type="Submit">Submit</Button>
+         </Form>
+      </div>
+   );
 }
--- src/components/ReactHookForm/ReactHookForm.component.js
+++ src/components/ReactHookForm/ReactHookForm.component.js
@@ -1,24 +1,32 @@
-import React from 'react';
-import { useForm } from 'react-hook-form';
+import React from "react";
+import { useForm } from "react-hook-form";
+import { Form, Button } from "react-bootstrap";
 
 export default function ReactHookFormComp() {
-    const { register, handleSubmit, watch, errors } = useForm();
-    const onSubmitHandler = data => { console.log(data) };
+   const { register, handleSubmit, watch, errors } = useForm();
+   const onSubmitHandler = data => {
+      console.log(data);
+   };
 
-    return (
-        <React.Fragment>
-            {/* "handleSubmit" will validate your inputs before invoking "onSubmit" */}
-            <form onSubmit={handleSubmit(onSubmitHandler)} >
-                {/* register your input into the hook by invoking the "register" function */}
-                <input name="example" defaultValue="test" ref={register} />
+   return (
+      <React.Fragment>
+         <div style={{ width: "80%" }}>
+            <Form onSubmit={handleSubmit(onSubmitHandler)}>
+               <Form.Group controlId="formBasicEmail">
+                  <Form.Label>Email address</Form.Label>
+                  <Form.Control type="email" placeholder="Enter email" name="email" ref={register} />
+                  <Form.Text className="text-muted">We'll never share your email with anyone else.</Form.Text>
+               </Form.Group>
 
-                {/* include validation with required or other standard HTML validation rules */}
-                < input name="exampleRequired" ref={register({ required: true })} />
-                {/* errors will return when field validation fails  */}
-                {errors.exampleRequired && <span>This field is required</span>}
-
-                <input type="submit" />
-            </form >
-        </React.Fragment>
-    )
+               <Form.Group controlId="formBasicPassword">
+                  <Form.Label>Password</Form.Label>
+                  <Form.Control type="password" placeholder="Password" name="password" ref={register({ minLength: 6 })} />
+               </Form.Group>
+               <Button variant="primary" type="submit">
+                  Submit
+               </Button>
+            </Form>
+         </div>
+      </React.Fragment>
+   );
 }
